689c285f6de393c4d7886d3c6a09b897edbf802f
Shaked Meirom <meiromshaked@gmail.com>
2017-12-23 13:26:01 +0200

    added high level heuristc for better player

--- wet2/players/better_player/__init__.py
+++ wet2/players/better_player/__init__.py
@@ -3,7 +3,8 @@ from utils import INFINITY, run_with_limited_time, ExceededTimeError
 from Reversi.consts import EM, OPPONENT_COLOR, BOARD_COLS, BOARD_ROWS
 import time
 import copy
-
+import math
+import numpy as np
 
 
 class Player(abstract.AbstractPlayer):
@@ -33,7 +34,7 @@ class Player(abstract.AbstractPlayer):
         for move in possible_moves:
             new_state = copy.deepcopy(game_state)
             new_state.perform_move(move[0],move[1])
-            if self.utility(new_state) > self.utility(next_state):
+            if self.heuristic(new_state) > self.heuristic(next_state):
                 next_state = new_state
                 best_move = move
 
@@ -46,8 +47,43 @@ class Player(abstract.AbstractPlayer):
 
         return best_move
 
+    def discs_diff(self, state):
+        my_u = 0
+        op_u = 0
+        for x in range(BOARD_COLS):
+            for y in range(BOARD_ROWS):
+                if state.board[x][y] == self.color:
+                    my_u += 1
+                if state.board[x][y] == OPPONENT_COLOR[self.color]:
+                    op_u += 1
 
-    def utility(self, state):
+        if my_u == 0:
+            # I have no tools left
+            return -INFINITY
+        elif op_u == 0:
+            # The opponent has no tools left
+            return INFINITY
+        else:
+            return my_u - op_u
+
+    def opponent_moves(self, state):
+        return len(state.get_possible_moves())
+
+    def heuristic(self, state):
+        if len(state.get_possible_moves()) == 0:
+            return INFINITY if state.curr_player != self.color else -INFINITY
+
+        parameters = np.asarray(self.discs_diff(state), self.opponent_moves(state))
+        weights = np.asarray([1, 0.5])
+
+        heuristic_value = sum(parameters * weights)
+        return heuristic_value
+
+
+
+
+
+
 
 
         
