da9c22b5bfb118bd65e96f312ddddd8a75a38f10
dorhugi <dorhugi@gmail.com>
2017-12-23 12:35:29 +0200

    added better player pakcage

---
+++ wet2/players/better_player/__init__.py
@@ -0,0 +1,47 @@
+import abstract
+from utils import INFINITY, run_with_limited_time, ExceededTimeError
+from Reversi.consts import EM, OPPONENT_COLOR, BOARD_COLS, BOARD_ROWS
+import time
+import copy
+
+
+
+class Player(abstract.AbstractPlayer):
+
+    def __init__(self, setup_time, player_color, time_per_k_turns, k):
+        abstract.AbstractPlayer.__init__(self, setup_time,
+                                         player_color, time_per_k_turns, k)
+        self.clock = time.time()
+
+        # We are simply providing (remaining time / remaining turns) for each turn in round.
+        # Taking a spare time of 0.05 seconds.
+        self.turns_remaining_in_round = self.k
+        self.time_remaining_in_round = self.time_per_k_turns
+        self.time_for_current_move = self.time_remaining_in_round / self.turns_remaining_in_round - 0.05
+
+    def get_move(self, game_state, possible_moves):
+        self.clock = time.time()
+        self.time_for_current_move = self.time_remaining_in_round / self.turns_remaining_in_round - 0.05
+        if len(possible_moves) == 1:
+            return possible_moves[0]
+
+        best_move = possible_moves[0]
+        next_state = copy.deepcopy(game_state)
+        next_state.perform_move(best_move[0],best_move[1])
+        # Choosing an arbitrary move
+        # Get the best move according the utility function
+        for move in possible_moves:
+            new_state = copy.deepcopy(game_state)
+            new_state.perform_move(move[0],move[1])
+            if self.utility(new_state) > self.utility(next_state):
+                next_state = new_state
+                best_move = move
+
+        if self.turns_remaining_in_round == 1:
+            self.turns_remaining_in_round = self.k
+            self.time_remaining_in_round = self.time_per_k_turns
+        else:
+            self.turns_remaining_in_round -= 1
+            self.time_remaining_in_round -= (time.time() - self.clock)
+
+        return best_move
